fd3d5554ce91ea337f6712f0c62613eee44bf77c
luialbeto <araujo.script@gmail.com>
2021-03-27 22:49:01 -0300

    Atualizaçao 3 Carreiras

--- src/test/java/io/cucumber/hopper/ListaCarreirasTestSteps.java
+++ src/test/java/io/cucumber/hopper/ListaCarreirasTestSteps.java
@@ -1,14 +1,11 @@
 package io.cucumber.hopper;
 
-import io.cucumber.hopper.Servicos.Config;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.hopper.Servicos.Config;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
-import io.cucumber.java.pt.Quando;
 
 public class ListaCarreirasTestSteps{
     @Dado("que eu estou no site da accenture 3")
@@ -18,7 +15,7 @@ public class ListaCarreirasTestSteps{
 
     @Dado("clico no menu carreiras")
     public void clico_no_menu_carreiras() {
-        WebElement input = Config.browser.findElement(By.cssSelector("div[class="expand-collapse-container visible-md visible-xs visible-sm"]"));
+        WebElement input = Config.browser.findElement(By.cssSelector("div[data-id='primaryLink4_Carreiras']"));
         input.click();
     }
 
@@ -30,12 +27,14 @@ public class ListaCarreirasTestSteps{
 
     @Dado("digito no campo de busca {string}")
     public void digito_no_campo_de_busca(String string) {
-
+        WebElement input = Config.browser.findElement(By.cssSelector("#search-form-label");
+        input.type("DESENVOLVEDOR");
     }
 
     @Dado("clico no botao procurar")
     public void clico_no_botao_procurar() {
-  
+        WebElement input = Config.browser.findElement(By.cssSelector(".search-text");
+        input.click();
     }
 
     @Entao("devo encontrar vagas para programadores")
